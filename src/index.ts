6c2edd18fa8499dcd59617a3e5911ca1ab3b0f27
Prathmesh Mulhar <prathmeshmulhar@gmail.com>
2025-07-09 23:05:13 +0530

    added changes for index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,28 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import productRoutes from './routes/productRoutes';
-import { config } from './config';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import productRoutes from "./routes/productRoutes";
+import { config } from "./config";
 
 const app = express();
-app.use(cors({
+app.use(
+  cors({
     origin: config.clientUri,
-  }));
+  })
+);
 app.use(express.json());
 
-mongoose.connect(config.mongodbUri)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(config.mongodbUri)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
-app.use('/api/products', productRoutes);
+app.use("/api/products", productRoutes);
 
-app.listen(config.port, () => console.log(`Server running on port ${config.port}`));
+app.listen(config.port, () =>
+  console.log(`Server running on port ${config.port}`)
+);
+
+app.get("/", (req, res) => {
+  res.json({ message: `Server running on port ${config.port}` });
+});
