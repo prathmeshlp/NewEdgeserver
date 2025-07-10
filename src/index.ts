d571fb35a72318056dc71f3b351918c4711b0787
Prathmesh Mulhar <prathmeshmulhar@gmail.com>
2025-07-10 10:37:05 +0530

    added changes for index file

--- src/index.ts
+++ src/index.ts
@@ -12,16 +12,32 @@ app.use(
 );
 app.use(express.json());
 
-mongoose
-  .connect(config.mongodbUri)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+// mongoose
+//   .connect(config.mongodbUri)
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use("/api/products", productRoutes);
 
-app.listen(config.port, () =>
-  console.log(`Server running on port ${config.port}`)
-);
+// app.listen(config.port, () =>
+//   console.log(`Server running on port ${config.port}`)
+// );
+
+let isConnected = false;
+
+async function connectDB() {
+  if (!isConnected) {
+    await mongoose.connect(config.mongodbUri);
+    isConnected = true;
+    console.log("MongoDB connected");
+  }
+}
+
+export default async function handler() {
+  await connectDB();
+}
+
+
 
 app.get("/", (req, res) => {
   res.send("Api is running");
